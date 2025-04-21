bb12e42f7b16d5055fe40effcbd8896aa47c5a37
dusy93 <dusy93@naver.com>
2025-04-21 17:11:33 +0900

    init

---
+++ ForMyDay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ForMyDay
+//
+//  Created by yeon on 4/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
