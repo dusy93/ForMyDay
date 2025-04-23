4ebfddebcd2dfc3d45004d3173ad671dc8fd565e
dusy93 <dusy93@naver.com>
2025-04-23 17:15:32 +0900

    group 추가

---
+++ ForMyDay/Presentation/MainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainViewController.swift
+//  ForMyDay
+//
+//  Created by yeon on 4/23/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
